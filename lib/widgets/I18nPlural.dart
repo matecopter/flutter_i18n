91383b7ebdc27fd4843591c29c86af9dfd904358
Christian Epping <christian.epping@mni.thm.de>
2020-07-09 10:30:12 +0200

    Modified `I18nPlural`-Widget to act more like a `Text`-Widget

--- lib/widgets/I18nPlural.dart
+++ lib/widgets/I18nPlural.dart
@@ -1,3 +1,4 @@
+import 'dart:ui' as ui show TextHeightBehavior;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 
@@ -7,7 +8,54 @@ class I18nPlural extends StatelessWidget {
   final Text child;
   static const _default_text = Text("");
 
-  I18nPlural(this._key, this._pluralValue, {this.child = _default_text});
+  I18nPlural(this._key, this._pluralValue, {this.child = _default_text})
+      : assert(
+          _key != null,
+          'A non-null String must be provided to an I18nPlural widget.',
+        ),
+        assert(_pluralValue != null,
+            'A non-null Integer must be provided to an I18nPlural widget.'),
+        assert(_pluralValue >= 0,
+            'An Integer equal to or greater than zero must be provided to an I18nPlural widget.');
+
+  I18nPlural.standalone(
+    this._key,
+    this._pluralValue, {
+    Key key,
+    TextStyle style,
+    StrutStyle strutStyle,
+    TextAlign textAlign,
+    TextDirection textDirection,
+    bool softWrap,
+    TextOverflow overflow,
+    double textScaleFactor,
+    int maxLines,
+    String semanticsLabel,
+    TextWidthBasis textWidthBasis,
+    ui.TextHeightBehavior textHeightBehavior,
+  })  : this.child = Text(
+          "",
+          key: key,
+          style: style,
+          strutStyle: strutStyle,
+          textAlign: textAlign,
+          textDirection: textDirection,
+          softWrap: softWrap,
+          overflow: overflow,
+          textScaleFactor: textScaleFactor,
+          maxLines: maxLines,
+          semanticsLabel: semanticsLabel,
+          textWidthBasis: textWidthBasis,
+          textHeightBehavior: textHeightBehavior,
+        ),
+        assert(
+          _key != null,
+          'A non-null String must be provided to an I18nPlural widget.',
+        ),
+        assert(_pluralValue != null,
+            'A non-null Integer must be provided to an I18nPlural widget.'),
+        assert(_pluralValue >= 0,
+            'An Integer equal to or greater than zero must be provided to an I18nPlural widget.');
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +72,7 @@ class I18nPlural extends StatelessWidget {
       maxLines: child.maxLines,
       semanticsLabel: child.semanticsLabel,
       textWidthBasis: child.textWidthBasis,
+      textHeightBehavior: child.textHeightBehavior,
     );
   }
 }
